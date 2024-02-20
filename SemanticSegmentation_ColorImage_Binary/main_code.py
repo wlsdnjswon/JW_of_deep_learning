36a31ad948b8fd7ecf59d9ab6beb1f0d8ecd25d5
정진원 <71718618+wlsdnjswon@users.noreply.github.com>
2024-02-20 14:16:19 +0900

    Update main_code.py

--- SemanticSegmentation_ColorImage_Binary/main_code.py
+++ SemanticSegmentation_ColorImage_Binary/main_code.py
@@ -7,8 +7,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 #데이터셋 위치
-image_directory = 'C:/testAI/1/'
-mask_directory = 'C:/testAI/2/'
+image_directory = './patch_image/'
+mask_directory = './patch_mask/'
 
 #데이터셋 설정
 SIZE = 512
@@ -33,17 +33,17 @@ for i, image_name in enumerate(masks):
         mask_dataset.append(np.array(image))
 
 #normalize 함수를 사용해서 정규화
-image_dataset = normalize(np.array(image_dataset), axis=(2,3))
+#image_dataset = normalize(np.array(image_dataset), axis=(2,3))
 
 #255로 나누어 0-1로 정규화
-#image_dataset = np.array(image_dataset) /255.
+image_dataset = np.array(image_dataset) /255.
 
 #마스크 정규화 expand_dims함수를 통해 2차원 이미지를 3차원형태로 만들어줌
 mask_dataset = np.expand_dims((np.array(mask_dataset)),3) /255.
 
 #image_dataset과 mask_dataset은 (데이터셋개수, 가로, 세로, 채널)형식으로 이루어져 있음
 
-#데이터 분할
+#데이터 분할 train 10% : test 10%
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(image_dataset, mask_dataset, test_size = 0.10, random_state = 0)
 
@@ -86,7 +86,7 @@ model.save('test.hdf5')
 #모델평가
 
 
-	# 모델 정확도
+# 모델 정확도
 _, acc = model.evaluate(X_test, y_test)
 print("Accuracy = ", (acc * 100.0), "%")
 
@@ -119,11 +119,10 @@ plt.show()
 ##################################
 #IOU
 
-y_pred=model.predict(X_train)
-y_pred_thresholded = y_pred[1] > 0.5
-y_test_thresholded = y_test[1]
+y_pred=model.predict(X_test)
+y_pred_thresholded = y_pred > 0.5
 
-intersection = np.logical_and(y_test_thresholded, y_pred_thresholded)
+intersection = np.logical_and(y_test, y_pred_thresholded)
 union = np.logical_or(y_test, y_pred_thresholded)
 iou_score = np.sum(intersection) / np.sum(union)
 print("IoU socre is: ", iou_score)
@@ -135,34 +134,38 @@ model.load_weights('test.hdf5')
 
 #test 셋에 있는 이미지 예측
 test_img_number = random.randint(0, len(X_test))
+
 test_img = X_test[test_img_number]
 ground_truth=y_test[test_img_number]
 test_img_input = np.expand_dims(test_img, 0)
-prediction = (model.predict(test_img_input)[0,:,:,0] > 0.2).astype(np.uint8)
+prediction = (model.predict(test_img_input)[0,:,:,0] > 0.5).astype(np.uint8)
 
-#외부 이미지를 가져와 예측 
-test_img_other = cv2.imread('C:/testAI/1/[0002]BtmBF0_12.jpg')
-test_img_other_norm = normalize(np.array(test_img_other), axis=(2,3))
-test_img_other_input = np.expand_dims(test_img_other_norm, 0)
-prediction_other = (model.predict(test_img_other_input)[0,:,:,:] > 0.2).astype(np.uint8)
-
-#해당 이미지를 출력
 plt.figure(figsize=(16, 8))
 plt.subplot(231)
 plt.title('Testing Image')
-plt.imshow(test_img, cv2.COLOR_BGR2RGB)
+plt.imshow(test_img[:,:,0], cmap='gray')
 plt.subplot(232)
 plt.title('Testing Label')
 plt.imshow(ground_truth[:,:,0], cmap='gray')
 plt.subplot(233)
 plt.title('Prediction on test image')
 plt.imshow(prediction, cmap='gray')
-plt.subplot(234)
-plt.title('External Image')
-plt.imshow(test_img_other, cv2.COLOR_BGR2RGB)
-plt.subplot(235)
-plt.title('Prediction of external Image')
-plt.imshow(prediction_other, cmap='gray')
+
+#외부 이미지를 가져와 예측 
+test_img_other = cv2.imread('./patch_image/[0001]BtmBF1_22.png',  cv2.COLOR_BGR2RGB)
+test_img_other_input = np.expand_dims(test_img_other,0)
+test_img_other_input = image_dataset = np.array(image_dataset) /255.
+#test_img_other_input = normalize(np.array(test_img_other_input), axis=(2,3))
+prediction_other = (model.predict(test_img_other_input)[0,:,:,:] > 0.1).astype(np.uint8)
+
+#해당 이미지를 출력
+plt.figure(figsize=(16, 8))
+plt.subplot(231)
+plt.title('Testing Image')
+plt.imshow(test_img_other)
+plt.subplot(232)
+plt.title('Testing Label')
+plt.imshow(prediction_other[:,:,0], cmap='gray')
 plt.show()
 
 
